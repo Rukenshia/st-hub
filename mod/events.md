0753d7994d3d2ab04f019ec1d3c8428f3bda8b69
jan <jan@ruken.pw>
2019-06-29 21:38:05 +0200

    More progress

---
+++ mod/events.md
@@ -0,0 +1,24 @@
+## SFM Events
+
+* `action.selectShip{shipId=Number}`
+* --- start battle --
+* --- end battle ---
+
+* action.onEnterDivision
+* action.leaveDivision
+* `dockState.balancer{queueTypeIdent=cooperative|random}`
+
+
+## python.log
+
+Battle results
+
+```plain
+2019-06-29 19:33:36.088: INFO: [Script] [Battle Results] {'credits': 85137, 'credits_compensation': 0, 'exp_penalty': 0, 'exp': 512, 'credits_penalty': 0}; (, 23765753418754, 1778)
+```
+
+Battle message
+
+```plain
+2019-06-29 19:33:37.717: INFO: [Script] [[MES] __addInfoToMessage] {'typeId': 5, 'exp': 2139, 'tasks': 'aogasFactor': 1.0, 'mapBGPath': '../maps_bg/28_naval_mission.png', 'operationName': '', 'rankStarsDeltaChange': 0, 'earnedAchievements': [], 'header': 'IDS_SYS_MESSAGE_HEADER_MS_ACC_BATTLE_RESULTS_1', 'result': 1, 'creditsCompensation': 0, 'uiSpecial': True, 'mapTypeId': 18, 'shipId': 3760109008L, 'accountLeveling': {'expGained': 1, 'currLevel': 15, 'prevLevel': 15, 'currLevelExp': 200, 'expTotal': 2234, 'nextLevelExp': 200}, 'curTasksCompleted': 0, 'mapName': 'IDS_SPACES/28_NAVAL_MISSION', 'battleTypeId': 5, 'shipLevelRome': 'X', 'achievements': (), 'isPremium': True, 'shipName': 'IDS_PRSC510', 'shipsKilled': 2.0, 'importance': 2, 'teamBuildTypeId': 2, 'idInGroup': 0, 'shipTypeIDS': 'IDS_CRUISER', 'difficulty': '', 'prevTasksCompleted': 0, 'isElite': False, 'date': '29.06.2019 19:26', 'dt': 1561829613, 'groupId': 3, 'accLeveling': (15, 15, 2233, 1), 'expPenalty': 0, 'arenaUniqueID': 8139338249903935L, 'credits': 153248, 'sourceId': 0, 'creditsPenalty': 0, 'viaMM': False, 'rankBattlesSeasonId': 0, 'shipType': 'Cruiser', 'quests': [], 'stypeIdent': 'Cruiser', 'postponed': False, 'battleCreateTime': 1561829183, 'rankDeltaChange': 0}; (, 49929965138026, 180)
+```
--- mod/src/Main.py
+++ mod/src/Main.py
@@ -1,7 +1,72 @@
 API_VERSION = 'API_v1.0'
 MOD_NAME = 'StHub'
 
-def test(*args, **kwargs):
-    print 'STHUB TEST CALLED'
+devmenu.enable()
 
-flash.addExternalCallback('stHubTest', test)
+ev_log = open('events.log', 'w')
+battle_log = open('battle.log', 'w')
+
+class StHub:
+    def __init__(self):
+        self.battle_damage = 0
+
+        events.onReceiveShellInfo(self.shell_info)
+        events.onBattleEnd(self.battle_end)
+        events.onBattleQuit(self.battle_quit)
+        events.onBattleStart(self.battle_start)
+
+    def shell_info(self, victim, shooter, ammo, mat, shoot, flags, damage, pos, yaw, hlinfo):
+        if flags & 0b1:
+            flash.call('sthub.LastEvent', ['StHub.Shell received damage %s'%(damage)])
+            wl(battle_log, 'shell: %s %s %s %s %s %s %s %s %s %s'%(victim, shooter, ammo, mat, shoot, '{0:b}'.format(flags), damage, pos, yaw, hlinfo))
+        else:
+            flash.call('sthub.LastEvent', ['StHub.Shell dealt damage %s'%(damage)])
+            wl(battle_log, 'shell: %s %s %s %s %s %s %s %s %s %s'%(victim, shooter, ammo, mat, shoot, '{0:b}'.format(flags), damage, pos, yaw, hlinfo))
+
+            self.battle_damage = self.battle_damage + damage
+        
+        flash.call('sthub.LastEvent', ['StHub.AmmoInfo %s'%(battle.getAmmoParams(ammo))])
+        wl(battle_log, 'ammo_info %s'%(battle.getAmmoParams(ammo)))
+
+    def battle_start(self):
+        wl(battle_log, '--- battle start')
+        flash.call('sthub.LastEvent', ['StHub.BattleStart'])
+
+        wl(open('battle.self.log', 'w'), '%s'%(battle.getSelfPlayerInfo()))
+        wl(open('battle.players.log', 'w'), '%s'%(battle.getPlayersInfo()))
+
+        self.battle_damage = 0
+
+    def battle_quit(self, _m):
+        flash.call('sthub.LastEvent', ['StHub.BattleQuit'])
+        wl(battle_log, '--- battle quit')
+
+        wl(open('battle.self_end.log', 'w'), '%s'%(battle.getSelfPlayerInfo()))
+        wl(open('battle.players_end.log', 'w'), '%s'%(battle.getPlayersInfo()))
+
+
+    def battle_end(self, a, b):
+        flash.call('sthub.LastEvent', ['StHub.BattleEnd %s %s'%(a, b)])
+        flash.call('sthub.LastEvent', ['StHub.AvgStats damage=%d'%(self.battle_damage)])
+    
+        wl(battle_log, '--- battle end (%s, %s)'%(a, b))
+
+
+def wl(l, m):
+    l.write('{}\n'.format(m))
+    l.flush()
+
+
+def set_last_event(name, data):
+    flash.call('sthub.LastEvent', ['{}: {}'.format(name, utils.jsonEncode(data, ensure_ascii=True))])
+
+    wl(ev_log, '{}\t{}'.format(name, utils.jsonEncode(data, ensure_ascii=True)))
+
+
+def flash_ready(_m): 
+    flash.call('sthub.CallMeBaby', [])
+
+
+events.onSFMEvent(set_last_event)
+events.onFlashReady(flash_ready)
+StHub()
